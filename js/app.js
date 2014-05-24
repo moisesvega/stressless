6e2436518e574903513cca208e9a2bf9d9fbe063
Moises Vega <moises.vega@gmail.com>
2014-05-24 18:30:51 -0500

    template

--- js/app.js
+++ js/app.js
@@ -2,10 +2,15 @@ App = Ember.Application.create();
 
 App.Router.map(function() {
   // put your routes here
+  this.resource('login');
+  this.resource('balllon');
+  this.resource('circulo');
+  this.resource('session');
+
 });
 
 App.IndexRoute = Ember.Route.extend({
   model: function() {
-    return ['red', 'yellow', 'blue'];
+    return ['red', 'yellow', 'blue', 'hey'];
   }
 });
