6125b93b14e94fb655ffcc29dd6262effd7e5405
Moises Vega <moises.vega@gmail.com>
2014-05-24 20:04:35 -0500

    Fix css facebook

--- js/libs/facebook.js
+++ js/libs/facebook.js
@@ -76,3 +76,28 @@
         'Thanks for logging in, ' + response.name + '!';
     });
   }
+
+  function fb_login(){
+    FB.login(function(response) {
+
+        if (response.authResponse) {
+            console.log('Welcome!  Fetching your information.... ');
+            //console.log(response); // dump complete info
+            access_token = response.authResponse.accessToken; //get access token
+            user_id = response.authResponse.userID; //get FB UID
+
+            FB.api('/me', function(response) {
+                user_email = response.email; //get user email
+          // you can store this data into your database             
+            });
+            checkLoginState();
+
+        } else {
+            //user hit cancel button
+            console.log('User cancelled login or did not fully authorize.');
+
+        }
+    }, {
+        scope: 'public_profile,email'
+    });
+}
